47bccedecf1527e2a3eaf8dceb50900354b4268b
minhdao <daominh91197@gmail.com>
2018-09-15 17:11:13 +0300

    structure app and setup boilerplate

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
 import "./index.css";
-import App from "./App";
+import reducers from "./reducers";
+import App from "./components/App";
+
 import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={createStore(reducers)}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
---
+++ src/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from "redux";
+
+const rootReducers = combineReducers({
+  state: (state = {}) => state
+});
+
+export default rootReducers;
